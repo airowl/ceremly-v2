a55a3eda03ce78c8b8c1ec96d0fcb5e237c1ae0b
airowl <airowl.gasga@gmail.com>
2026-06-05 15:55:46 +0200

    refactor: delete event service, team service, event-scoped RBAC and routes (phase 1a)

--- server/utils/event.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Re-export from event.service.ts for backwards compatibility.
- * New code should import directly from '~~/server/services/event.service'
- */
-export {
-    requireEventOwnership,
-    generateEventSlug,
-} from "../services/event.service";
