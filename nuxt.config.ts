80f597e6f9e9e7550965622373c0e4ff4e715055
airowl <airowl.gasga@gmail.com>
2026-07-24 15:01:55 +0200

    fix(schema): Organization logo as absolute URL

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -286,7 +286,7 @@ export default defineNuxtConfig({
             type: "Organization",
             name: process.env.NUXT_PUBLIC_APP_NAME || "",
             url: process.env.NUXT_PUBLIC_BASE_URL || "",
-            logo: "/icon.png",
+            logo: `${process.env.NUXT_PUBLIC_BASE_URL || ""}/icon.png`,
         },
     },
 
